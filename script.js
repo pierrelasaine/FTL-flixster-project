27bde7c05cd3f5d65482f5ddc76fe28e4dd1fc91
Pierre Johnson <pierre.johnson@pierrej-ltm7bed.internal.salesforce.com>
2023-06-09 12:05:17 -0700

    completed site in need of fine tuning

--- script.js
+++ script.js
@@ -6,9 +6,18 @@ const options = {
     }
   }
 
-const imageBaseUrl = 'https://image.tmdb.org/t/p/original'
+const imageBaseUrl = "https://image.tmdb.org/t/p/original"
+const nowPlayingBaseUrl = "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page="
+const searchBaseUrl = "https://api.themoviedb.org/3/search/movie?query="
 
-const nowPlayingUrl = "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page="
+const searchButton = document.querySelector("#search-button")
+const searchInput = document.querySelector("#movie-search")
+const loadMoreMoviesButton = document.querySelector("#load-more-movies-btn")
+const resultsTitleBox = document.querySelector(".results-title")
+const resultsTitle = document.querySelector(".results-title b")
+const mainDiv = document.querySelector(".movies-grid")
+const posterPlaceholder = "https://via.placeholder.com/720x1080/eee?text=%20%E2%80%8C"
+const backButton = document.createElement("button")
 
 function generateCard(movieObject, targetDiv) {
     // Expected format:
@@ -23,34 +32,36 @@ function generateCard(movieObject, targetDiv) {
     //     </section>
 
     // Create movie card section
-    let movieCard = document.createElement("section");
+    const movieCard = document.createElement("section");
     movieCard.classList.add("movie-card");
 
     // Create image element (src attribute can be added later)
-    let movieImage = document.createElement("img");
+    const movieImage = document.createElement("img");
     movieImage.classList.add("movie-poster")
+    movieImage.src = imageBaseUrl.concat(movieObject.poster_path)
+
     movieImage.src = imageBaseUrl.concat(movieObject.poster_path)
     movieCard.appendChild(movieImage);
 
     // Create and append movie rating div
-    let movieRating = document.createElement("div");
+    const movieRating = document.createElement("div");
     movieRating.classList.add("movie-votes");
     movieCard.appendChild(movieRating);
 
     // Create and append star span to movie rating div
-    let star = document.createElement("span");
+    const star = document.createElement("span");
     star.classList.add("star");
     star.innerText = "⭐️ ";
     movieRating.appendChild(star);
 
     // Create and append rating value span to movie rating div
-    let ratingValue = document.createElement("span");
+    const ratingValue = document.createElement("span");
     ratingValue.classList.add("rating-value");
     ratingValue.innerText = movieObject.vote_average;
     movieRating.appendChild(ratingValue);
 
     // Create and append movie title div
-    let movieTitle = document.createElement("div");
+    const movieTitle = document.createElement("div");
     movieTitle.classList.add("movie-title");
     movieTitle.innerText = movieObject.original_title;
     movieCard.appendChild(movieTitle);
@@ -66,21 +77,21 @@ function generateRow() {
     //     </section>
 
     // Create movie row section
-    let movieRow = document.createElement("section")
+    const movieRow = document.createElement("section")
     movieRow.classList.add("movie-row")
 
-    let mainDiv = document.querySelector(".movies-grid")
     mainDiv.appendChild(movieRow)
 
     return movieRow
 }
 
-function generateMoviePage(page) {
-    fetch(nowPlayingUrl.concat(`${page}`), options)
+function generateMoviePage(url, page) {
+    // Generate movie page data from the TMDB API
+    fetch(url.concat(page), options)
         .then(response => response.json())
         .then(moviesData => {
-            let movies = moviesData.results
-            let targetDiv = generateRow()
+            const movies = moviesData.results
+            let targetDiv = generateRow() // first row generated on assignment
 
             movies.forEach(function(movie, index) {
             if (index % 4 === 0) {
@@ -92,13 +103,47 @@ function generateMoviePage(page) {
     })
 }
 
+function generateSearchResults(keyword, needBackButton) {
+    if (needBackButton === true) {
+
+        resultsTitleBox.insertBefore(backButton, resultsTitle)
+
+        needBackButton = false
+    }
+
+
+    resultsTitle.innerText = `Keyword: ${keyword}`
+
+    keyword = keyword.replace(" ", "+")
+    currentUrl = `${searchBaseUrl}${keyword}&page=`
+
+    generateMoviePage(currentUrl, 1)
+}
+
 // Load first page
 let page = 1
-generateMoviePage(page)
+let needBackButton = true;
+currentUrl = nowPlayingBaseUrl
+generateMoviePage(currentUrl, page)
 
-// On button press, load next page
-let loadMoreMoviesButton = document.querySelector("#load-more-movies-btn")
+// On button press, load next page given the current URL
 loadMoreMoviesButton.addEventListener("click", () => {
     page += 1
-    generateMoviePage(page)
+    generateMoviePage(currentUrl, page)
+})
+
+// On button press, remove current results and generate search results
+searchButton.addEventListener("click", (event) => {
+    event.preventDefault()
+    mainDiv.innerHTML = ""
+
+    let keyword = searchInput.value
+    generateSearchResults(keyword, needBackButton)
+})
+
+backButton.classList.add("back")
+backButton.innerText = "⏪"
+
+backButton.addEventListener("click", () => {
+    location.reload()
 })
